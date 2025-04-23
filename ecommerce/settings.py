b34fef81c534355062af48a3c2af8d2ce186ecae
jossyking08 <jossyking17@gmail.com>
2025-04-23 13:46:28 +0100

    updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
      'localhost',
     '127.0.0.1',
     'ec2-13-60-76-111.eu-north-1.compute.amazonaws.com',
-    '13.60.76.111',  # Optional: Add the IP address too
+    '16.170.237.116',  # Optional: Add the IP address too
 ]
 
 
